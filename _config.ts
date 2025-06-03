9dcef7f508fb11127cc55eda344d11092d77ae75
Lex Feathers <lex@gameaudio.ca>
2025-06-03 11:58:14 -0400

    Added plugin setup

--- _config.ts
+++ _config.ts
@@ -3,11 +3,17 @@ import metas from "lume/plugins/metas.ts";
 import favicon from "lume/plugins/favicon.ts";
 import date from "lume/plugins/date.ts";
 
-const site = lume();
+const site = lume({
+  location: new URL("https://austra-site.netlify.app")
+});
+site.data("name", "Austra");
 site.use(favicon({
   input: "/assets/favicon.ico",
 }));
 site.use(date());
 site.use(metas());
+
+site.add("./assets");
+site.add("./styles.css");
 
 export default site;
