7eaf070c3f98c2c7ef4f1e4c4d587758c58f6156
Lex Feathers <lex@gameaudio.ca>
2025-06-03 11:50:09 -0400

    Added plugins and updated footer

--- _config.ts
+++ _config.ts
@@ -1,5 +1,13 @@
 import lume from "lume/mod.ts";
+import metas from "lume/plugins/metas.ts";
+import favicon from "lume/plugins/favicon.ts";
+import date from "lume/plugins/date.ts";
 
 const site = lume();
+site.use(favicon({
+  input: "/assets/favicon.ico",
+}));
+site.use(date());
+site.use(metas());
 
 export default site;
