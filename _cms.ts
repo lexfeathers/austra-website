41b98771d2d948313da2a597417dc80d72e06649
Lex Feathers <lex@gameaudio.ca>
2025-06-03 11:34:52 -0400

    Adding site

---
+++ _cms.ts
@@ -0,0 +1,92 @@
+import lumeCMS from "lume/cms/mod.ts";
+import GitHub from "lume/cms/storage/github.ts";
+import { Octokit } from "npm:octokit";
+
+// Set site time zone
+// Timezone codes can be found at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+Deno.env.set("TZ", "US/Eastern");
+
+// Get current date in YYYY-MM-DD format
+const date = new Date();
+const formattedDate = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+
+const username = Deno.env.get("USERNAME");
+const password = Deno.env.get("PASSWORD");
+
+const cms = lumeCMS({
+  site: {
+    name: "Austra",
+    url: "https://austra-site.netlify.app",
+  },
+  auth: {
+    method: "basic",
+    users: {
+      [username]: password,
+    },
+  },
+});
+
+cms.storage(
+  "src",
+  new GitHub({
+    client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+    owner: "lexfeathers",
+    repo: "featherdroid",
+  })
+);
+cms.upload("images", "src:assets/images");
+
+cms.collection({
+  name: "posts",
+  store: "src:posts/*.md",
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      description: "Leave blank to use published date",
+      value: formattedDate,
+    },
+    {
+      name: "author",
+      type: "text",
+      init(field) {
+        field.value = "Austra";
+      },
+    },
+    {
+      name: "draft",
+      type: "checkbox",
+    },
+    {
+      name: "date",
+      type: "datetime",
+      init(field) {
+        field.value = new Date("yyyy, MM, dd");
+      },
+    },
+    {
+      name: "excerpt",
+      type: "textarea",
+      attributes: {
+        maxlength: 800,
+      },
+    },
+    {
+      name: "content",
+      type: "markdown",
+      upload: "images",
+      label: "Post content",
+      value: "Write **markdown** code here",
+      description: `<a target="_blank" href="https://www.markdownguide.org">More info about markdown syntax</a>`,
+    },
+  ],
+  documentName: "{title}.md",
+});
+cms.document({
+  name: "Index",
+  description: "Edit the content of the homepage",
+  store: "src:index.md",
+  fields: ["title: text", "excerpt: text", "content: markdown"],
+});
+
+export default cms;
